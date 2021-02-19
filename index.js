69d702c7dec61a1daa6d1af32631e91f519598da
Ben Teichman <ben@voiceflow.com>
2021-02-19 16:52:00 -0500

    fix: add add missing production dependency

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = {
   rules: {
     'type-enum': [2, 'always', ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'merge', 'perf', 'refactor', 'revert', 'style', 'test', 'wip']],
